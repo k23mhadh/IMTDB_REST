902e2758039bb980a023e660d2644e489b7b00f0
oussama <b_soussema@yahoo.com>
2024-10-16 11:43:01 +0200

    Add the showtime service

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -14,6 +14,19 @@ with open('{}/databases/times.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
 
+@app.route("/showtimes", methods=['GET'])
+def get_schedule():
+    res = make_response(jsonify(schedule), 200)
+    return res
+
+@app.route("/showmovies/<date>", methods=['GET'])
+def get_movies_bydate(date):
+    for day in schedule:
+        if str(day["date"]) == str(date):
+            res = make_response(jsonify(day),200)
+            return res
+    return make_response(jsonify({"error":"No movies found for the specified date"}),400)
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
